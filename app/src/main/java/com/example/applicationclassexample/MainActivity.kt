47c284e6ab9ee4d8978cc1894e915fa77fd8689d
Ashesh Bharadwaj <ppashesh@gmail.com>
2019-11-21 13:21:08 +0530

    Initial Commit

--- app/src/main/java/com/example/applicationclassexample/MainActivity.kt
+++ app/src/main/java/com/example/applicationclassexample/MainActivity.kt
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Timber.i("onCreate() called")
+        Timber.i("onCreate() called from Timber")
     }
 }
